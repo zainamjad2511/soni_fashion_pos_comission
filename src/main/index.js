710f91f898a1b7dfc45280b15626e9e9f60b1c05
Zain Amjad <zainamjad2511@gmail.com>
2026-06-28 13:45:11 +0500

    feat: disable electron sandbox for linux compatibility

--- src/main/index.js
+++ src/main/index.js
@@ -3,6 +3,10 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
+if (process.platform === 'linux') {
+  app.commandLine.appendSwitch('no-sandbox')
+}
+
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
